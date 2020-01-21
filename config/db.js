c2b7556c9b8c89c43aba08ae348a7a08478b3a6b
Mac <os@Macs-MacBook-Pro.local>
2020-01-21 14:42:08 +0700

    test

--- config/db.js
+++ config/db.js
@@ -12,7 +12,7 @@ const connectDB = async () => {
     console.log("MongoDB connected...");
   } catch (err) {
     console.error(err.message);
-    //Exit process with failure
+    //Exit process with failure,
     process.exit(1);
   }
 };
